10bea99a93a9cabd527cd473551edce64485931e
Rafi-Siddiki <86559450+Rafi-Siddiki@users.noreply.github.com>
2025-04-11 10:03:05 +0600

    reg and login
    registration and login functionality added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
@@ -25,9 +28,12 @@ function AppLayout() {
 
 function App() {
   return (
+    <>
     <Router>
       <AppLayout />
     </Router>
+    <ToastContainer />
+    </>
   );
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import '../stylesheets/Login.css';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Login() {
   const [formData, setFormData] = React.useState({
@@ -11,6 +17,24 @@ function Login() {
   });
 
   const { email, password} = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  
+  const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+    useEffect(() => {
+      if (isError) {
+        toast.error(message);
+      }
+  
+      if (isSuccess || user) {
+        navigate('/');
+      }
+  
+      dispatch(reset());
+  
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
   
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -21,6 +45,16 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    }
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />;
   }
 
   return (
